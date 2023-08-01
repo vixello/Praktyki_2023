30b2b69dd3682e6c8500bfefac3a70b92568d552
Izabela K <aizeilla12344@gmail.com>
2023-08-01 10:13:14 +0200

    Task done

---
+++ Assets/objectB.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class objectB : MonoBehaviour
+{
+    public GameObject objectA;
+    public float a = 1f;
+    public float b = 2f;
+    public float delta = Mathf.PI / 2f;
+    float time = 0f;
+    float movementSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        LissajousMovement();
+    }
+    void LissajousMovement()
+    {
+
+
+
+        float radiusOfObjectA = objectA.GetComponent<objectA>().r;
+        Debug.Log("Radius of Object A: " + radiusOfObjectA);
+        
+   
+        time += Time.deltaTime * movementSpeed;
+        // x = Acos(at + delta)
+        // y = Bcos(bt)
+        //A and B are 1f; 
+
+        float x = (radiusOfObjectA - 0.25F) * Mathf.Cos(a * time + delta);
+        float y = Mathf.Sin(b * time);
+
+        Vector3 positionOfA = objectA.transform.position;
+        Vector3 offset = new Vector3(x, y, 0f);
+
+        transform.position = offset;
+
+    }
+}
